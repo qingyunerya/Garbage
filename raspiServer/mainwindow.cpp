9b47111798a505222c07330c3c801b0e3486d553
qingyunerya <2629615164@qq.com>
2019-06-04 20:38:15 +0800

    提交raspiClient

--- raspiServer/mainwindow.cpp
+++ raspiServer/mainwindow.cpp
@@ -5,6 +5,8 @@
 #include <QTime>
 #include <QTextCursor>
 #include <QMetaMethod>
+#include <QTimer>
+#include  <QGraphicsOpacityEffect>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,6 +15,32 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->tcp_server = new QTcpServer(this);
     connect(tcp_server,SIGNAL(newConnection()),this,SLOT(newConnection()));
+    data_timer=new QTimer(this);
+    data_timer->setInterval(100);
+    connect(data_timer,SIGNAL(timeout()),this,SLOT(data_timer_tick()));
+    commandList.append("control_key1_click");
+    commandList.append("control_key2_click");
+    commandList.append("control_key3_click");
+    commandList.append("control_led1_click");
+    commandList.append("control_led2_click");
+    commandList.append("control_led3_click");
+    commandList.append("control_readKeys_click");
+    commandList.append("control_directControl_click");
+    commandList.append("control_recLedOp_click");
+    commandList.append("control_recOp_click");
+    commandList.append("control_viewLedOp_click");
+    commandList.append("control_viewOp_click");
+    reCommandList.append("remote_status_isReadkeys:");
+    reCommandList.append("remote_status_isdirectControl:");
+    reCommandList.append("remote_status_l1:");
+    reCommandList.append("remote_status_l2:");
+    reCommandList.append("remote_status_l3:");
+    ui->groupBox->setEnabled(false);
+    QPixmap tb=QPixmap(":/Background/tb");
+    tb.scaled(ui->centralWidget->size(),Qt::KeepAspectRatio);
+    ui->centralWidget->setAutoFillBackground(true);
+    palette.setBrush(QPalette::Window,QBrush(tb));
+    ui->centralWidget->setPalette(palette);
 }
 
 MainWindow::~MainWindow()
@@ -29,6 +57,12 @@ void MainWindow::debug(QString str)
     ui->textEdit_console->setText(consoleStr);
     ui->textEdit_console->moveCursor(QTextCursor::End,QTextCursor::MoveAnchor);
 }
+
+void MainWindow::debug2(QString str)
+{
+    ui->textEdit_console_2->setText(str);
+    ui->textEdit_console_2->moveCursor(QTextCursor::End,QTextCursor::MoveAnchor);
+}
 //监听端口
 void MainWindow::on_connectToClient_clicked()
 {
@@ -39,6 +73,7 @@ void MainWindow::on_connectToClient_clicked()
     {
         tcp_server->listen(QHostAddress::Any,port);
         if(tcp_server->isListening())ui->connectToClient->setText("停止监听");
+        data_timer->start(0);
         debug("监听端口"+sport+"中");
     }
     else
@@ -60,6 +95,7 @@ void MainWindow::on_connectToClient_clicked()
                debug("清理socket"+QString::number(i,10));
             }
         }
+        data_timer->stop();
         this->ClientSocketList.clear();
         debug("已停止监听");
     }
@@ -83,20 +119,100 @@ void MainWindow::newConnection()
         this->client=client_socket;
         }
     }
+    ui->groupBox->setEnabled(true);
+    isRemoteControl=true;
 }
 
 //服务端发送数据
 void MainWindow::on_s_send_clicked()
 {
-    this->client->write(ui->textEdit_ss->toPlainText().toLocal8Bit());
+    QString time;
+    QTime current_time =QTime::currentTime();
+    time=current_time.toString("hh:mm:ss.zzz");
+    QString str=time+ui->textEdit_ss->toPlainText();
+    this->client->write(str.toUtf8());
 }
 
 //接受到客户端数据
 void MainWindow::ReceiveData()
 {
     debug("接受到来自"+client->peerAddress().toString()+"\n"+"客户端的数据");
-    QString sarr=QString::fromLocal8Bit(client->readAll());
-    ui->textEdit_sa->setText(client->peerAddress().toString()+"\n"+sarr);
+    QString sarr=QString::fromUtf8(client->readAll());
+    ui->textEdit_sa->setText(sarr);
+    ui->textEdit_sa->moveCursor(QTextCursor::End,QTextCursor::MoveAnchor);
+    if(acceptCommand().contains("remote_status_"))
+    {
+        curCommand=acceptCommand();
+        if(curCommand.compare(befCommand,Qt::CaseSensitive)!=0){befCommand=curCommand;}
+
+        if(isRemoteControl)
+        {
+            int index=0;
+            foreach (QString command,reCommandList)
+            {
+                if(curCommand.contains(command))
+                {
+                    index=reCommandList.indexOf(command);
+                }
+            }
+            switch (index)
+            {
+            case 0:
+                if(curCommand.contains("true"))
+                {
+                    ui->pushButton_readKeys->setText("停止读取");
+
+                }
+                else {ui->pushButton_readKeys->setText("读取按钮状态：");}
+                break;
+            case 1:
+                if(curCommand.contains("true"))
+                {
+                    ui->pushButton_directControl->setText("停止直控");
+
+                }
+                else {ui->pushButton_directControl->setText("直接控制灯：");}
+                break;
+            case 2:
+                if(curCommand.contains("true"))
+                {
+                    l1=true;
+                    palette.setColor(QPalette::Button,QColor(255,255,0));
+                    ui->pushButton_led1->setFlat(true);
+                    ui->pushButton_led1->setAutoFillBackground(true);
+                    ui->pushButton_led1->setPalette(palette);
+
+                }
+                else {ui->pushButton_led1->setAutoFillBackground(false);l1=false;}
+                break;
+            case 3:
+                if(curCommand.contains("true"))
+                {
+                    palette.setColor(QPalette::Button,QColor(255,255,0));
+                    ui->pushButton_led2->setFlat(true);
+                    ui->pushButton_led2->setAutoFillBackground(true);
+                    ui->pushButton_led2->setPalette(palette);
+                    l2=true;
+                }
+                else {ui->pushButton_led2->setAutoFillBackground(false);l2=false;}
+                break;
+            case 4:
+                if(curCommand.contains("true"))
+                {
+                    palette.setColor(QPalette::Button,QColor(255,255,0));
+                    ui->pushButton_led3->setFlat(true);
+                    ui->pushButton_led3->setAutoFillBackground(true);
+                    ui->pushButton_led3->setPalette(palette);
+                    l3=true;
+                }
+                else {ui->pushButton_led3->setAutoFillBackground(false);l3=false;}
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    else debug2(acceptCommand());
 }
 //断开服务端连接
 void MainWindow::disconnected()
@@ -110,9 +226,32 @@ void MainWindow::disconnected()
         client->destroyed();
         }
     }
-    debug(client->peerAddress().toString()+"已断开服务端连接");
-}
+    if(ui->comboBox->currentText().trimmed().isEmpty())
+    {
+        ui->groupBox->setEnabled(false);
+        debug(client->peerAddress().toString()+"已断开服务端连接");
+        isRemoteControl=false;
+        ui->textEdit_sa->clear();
+        ui->textEdit_ss->clear();
+    }
 
+}
+//发送命令
+void MainWindow::sendCommand(QString str)
+{
+    QString time;
+    QTime current_time =QTime::currentTime();
+    time=current_time.toString("hh:mm:ss.zzz");
+    str=time+":"+str;
+    QByteArray ba=str.toUtf8();
+    this->client->write(ba);
+}
+//接收命令
+QString MainWindow::acceptCommand()
+{
+    QString carr=ui->textEdit_sa->toPlainText();
+    return carr;
+}
 void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
     QTcpSocket *client_socket;
@@ -124,53 +263,140 @@ void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
     }
     debug("与"+arg1+"通信中");
 }
-
-void MainWindow::on_pushButton_readKeys_clicked()
+void MainWindow::data_timer_tick()
 {
 
-}
-
-void MainWindow::on_pushButton_directControl_clicked()
-{
-
-}
-
-void MainWindow::on_pushButton_recLed_clicked()
-{
-
-}
-
-void MainWindow::on_pushButton_recOperate_clicked()
-{
-    debug("dianji");
 }
 
 void MainWindow::on_pushButton_key1_clicked()
 {
-
+    sendCommand(commandList.at(0));
+    if(isControlLed)
+    {
+        if(!l1)
+        {
+            palette.setColor(QPalette::Button,QColor(255,255,0));
+            ui->pushButton_led1->setFlat(true);
+            ui->pushButton_led1->setAutoFillBackground(true);
+            ui->pushButton_led1->setPalette(palette);
+            l1=true;
+        }
+        else {ui->pushButton_led1->setAutoFillBackground(false);l1=false;}
+    }
 }
 
 void MainWindow::on_pushButton_key2_clicked()
 {
-
+    sendCommand(commandList.at(1));
+    if(isControlLed)
+    {
+        if(!l2)
+        {
+            palette.setColor(QPalette::Button,QColor(255,255,0));
+            ui->pushButton_led2->setFlat(true);
+            ui->pushButton_led2->setAutoFillBackground(true);
+            ui->pushButton_led2->setPalette(palette);
+            l2=true;
+        }
+        else {ui->pushButton_led2->setAutoFillBackground(false);l2=false;}
+    }
 }
 
 void MainWindow::on_pushButton_key3_clicked()
 {
-
+    sendCommand(commandList.at(2));
+    if(isControlLed)
+    {
+        if(!l3)
+        {
+            palette.setColor(QPalette::Button,QColor(255,255,0));
+            ui->pushButton_led3->setFlat(true);
+            ui->pushButton_led3->setAutoFillBackground(true);
+            ui->pushButton_led3->setPalette(palette);
+            l3=true;
+        }
+        else {ui->pushButton_led3->setAutoFillBackground(false);l3=false;}
+    }
 }
 
 void MainWindow::on_pushButton_led1_clicked()
 {
-
+    sendCommand(commandList.at(3));
+    if(!l1)
+    {
+        palette.setColor(QPalette::Button,QColor(255,255,0));
+        ui->pushButton_led1->setFlat(true);
+        ui->pushButton_led1->setAutoFillBackground(true);
+        ui->pushButton_led1->setPalette(palette);
+        l1=true;
+    }
+    else {ui->pushButton_led1->setAutoFillBackground(false);l1=false;}
 }
 
 void MainWindow::on_pushButton_led2_clicked()
 {
-
+    sendCommand(commandList.at(4));
+    if(!l2)
+    {
+        palette.setColor(QPalette::Button,QColor(255,255,0));
+        ui->pushButton_led2->setFlat(true);
+        ui->pushButton_led2->setAutoFillBackground(true);
+        ui->pushButton_led2->setPalette(palette);
+        l2=true;
+    }
+    else {ui->pushButton_led2->setAutoFillBackground(false);l2=false;}
 }
 
 void MainWindow::on_pushButton_led3_clicked()
 {
+    sendCommand(commandList.at(5));
+    if(!l3)
+    {
+        palette.setColor(QPalette::Button,QColor(255,255,0));
+        ui->pushButton_led3->setFlat(true);
+        ui->pushButton_led3->setAutoFillBackground(true);
+        ui->pushButton_led3->setPalette(palette);
+        l3=true;
+    }
+    else {ui->pushButton_led3->setAutoFillBackground(false);l3=false;}
+}
+void MainWindow::on_pushButton_readKeys_clicked()
+{
+    if(!isReadKeys)
+    {
+        ui->pushButton_readKeys->setText("停止读取");
+        sendCommand(commandList.at(6));
+        isReadKeys=true;
+    }
+    else {ui->pushButton_readKeys->setText("读取按钮状态");sendCommand(commandList.at(6));isReadKeys=false;}
+}
 
+void MainWindow::on_pushButton_directControl_clicked()
+{
+    if(!isControlLed)
+    {
+        ui->pushButton_directControl->setText("停止直控");
+        sendCommand(commandList.at(7));
+        isControlLed=true;
+    }
+    else {ui->pushButton_directControl->setText("直接控制灯：");sendCommand(commandList.at(7));isControlLed=false;}
+}
+void MainWindow::on_pushButton_recLed_clicked()
+{
+    sendCommand(commandList.at(8));
+}
+
+void MainWindow::on_pushButton_recOperate_clicked()
+{
+    sendCommand(commandList.at(9));
+}
+
+void MainWindow::on_pushButton_viewLedOp_clicked()
+{
+    sendCommand(commandList.at(10));
+}
+
+void MainWindow::on_pushButton_viewOp_clicked()
+{
+    sendCommand(commandList.at(11));
 }
--- raspiServer/mainwindow.h
+++ raspiServer/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 class QTcpSocket;
 class QTcpServer;
+class QTimer;
 namespace Ui {
 class MainWindow;
 }
@@ -16,10 +17,25 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     void debug(QString str);
+    void debug2(QString str);
+    QTimer *data_timer;
+    QList<QString> commandList;
+    QList<QString> reCommandList;
     QList<QTcpSocket *> ClientSocketList;
     QTcpServer *tcp_server;
     QTcpSocket *client;
     QString consoleStr;
+    QString consoleStr2;
+    QString curCommand;
+    QString befCommand="null";
+    bool isStartReadKeys=false;
+    void sendCommand(QString str);
+    QString acceptCommand();
+    bool isReadKeys=false;
+    bool isControlLed=false;
+    bool isRemoteControl=false;
+    QPalette palette;
+    bool l1=false,l2=false,l3=false;
 
 private slots:
 
@@ -28,7 +44,7 @@ private slots:
     void disconnected();
     void on_connectToClient_clicked();
     void on_s_send_clicked();
-
+    void data_timer_tick();
     void on_comboBox_currentIndexChanged(const QString &arg1);
 
     void on_pushButton_readKeys_clicked();
@@ -38,10 +54,6 @@ private slots:
     void on_pushButton_recLed_clicked();
 
     void on_pushButton_recOperate_clicked();
-
-    void on_label_key1_linkActivated(const QString &link);
-
-    void on_label_key2_linkHovered(const QString &link);
 
     void on_pushButton_key1_clicked();
 
@@ -54,6 +66,10 @@ private slots:
     void on_pushButton_led2_clicked();
 
     void on_pushButton_led3_clicked();
+
+    void on_pushButton_viewLedOp_clicked();
+
+    void on_pushButton_viewOp_clicked();
 
 private:
     Ui::MainWindow *ui;
